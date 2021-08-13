2eb3edead9f3fee393f4face00cd48c77af2d968
ShuxinD <shuxind11@gmail.com>
2021-08-13 14:48:35 -0400

    update age group count to older than 18

--- codes/01convertMAdeath_ZCTA.R
+++ codes/01convertMAdeath_ZCTA.R
@@ -1,10 +1,11 @@
-###########################################################################
+##################################################################
 # Project: beta radiation and CVD death in MA
 # Code: convert MA death record from one-row-per-person to one-row-per-zcta
 # Input: "ma_2000_2015.sas7bdat"
-# Output: "MAdeath_count_ZIP.rds"
+# Output: "zcta_year.rds" as combination of zcta and year
+# Output: "MAdeath_count_ZIP_age'group'.rds"
 # Author: Shuxin Dong
-##########################################################################
+##################################################################
 
 ## 0. set up --------------------------------------------------------------
 rm(list = ls())
@@ -71,32 +72,35 @@ origin <- origin[year(date) %in% 2001:2015,]
 dim(origin)
 # [1] 752805     16
 
+## only 18+
+origin <- origin[age>=18,]
+
 ## remove missing
 cat("Number of individual missing location info:", sum(is.na(origin[,lat])), "/", dim(origin)[1], sum(is.na(origin[,lat]))/dim(origin)[1])
-# Number of individual missing location info: 13636 / 752805 0.01811359
+# Number of individual missing location info: 13477 / 743873 0.01811734
 cat("Number of individual missing location info:", sum(is.na(origin[,long])), "/", dim(origin)[1], sum(is.na(origin[,long]))/dim(origin)[1])
-# Number of individual missing location info: 13636 / 752805 0.01811359
+# Number of individual missing location info: 13477 / 743873 0.01811734
 cat("Number of individual missing ICD info:", sum(origin[,icd]==""), "/", dim(origin)[1], sum(origin[,icd]=="")/dim(origin)[1])
-# Number of individual missing ICD info: 270 / 752805 0.0003586586
+# Number of individual missing ICD info: 259 / 743873 0.0003481777
 cat("Number of individual missing age info:", sum(is.na(origin[,age])), "/", dim(origin)[1], sum(is.na(origin[,age]))/dim(origin)[1])
-# Number of individual missing age info: 1564 / 752805 0.002077563
+# Number of individual missing age info: 0 / 743873 0
 
-## missingness for loc and ICD
+## missingness for location and ICD
 cat("Number of individual missing location info or ICD or:", sum(is.na(origin[,lat])| origin[,icd]=="" | is.na(origin[,age])), "/", dim(origin)[1], sum(is.na(origin[,lat])| origin[,icd]=="" | is.na(origin[,age]))/dim(origin)[1])
-# Number of individual missing location info or ICD or: 15422 / 752805 0.02048605
+# Number of individual missing location info or ICD or: 13722 / 743873 0.0184467
 
 mort <- na.omit(origin[,.(year, date, age, icd, lat, long, ID)])
 mort <- mort[icd != "",]
 dim(mort)
-# [1] 737383      7
+# [1] 730151      7
 head(mort)
-# year       date  icd      lat      long    ID
-# 1: 2001 2001-04-01 J449 42.08182 -72.61861 52758
-# 2: 2001 2001-04-01  F03 42.07359 -72.60768 52759
-# 3: 2001 2001-04-01 J449 42.05481 -72.62351 52760
-# 4: 2001 2001-08-01 I219 42.08514 -72.62851 52761
-# 5: 2001 2001-12-01  F03 42.08185 -72.61878 52762
-# 6: 2001 2001-02-01  I64 42.08078 -72.59399 52763
+# year       date age  icd      lat      long    ID
+# 1: 2001 2001-04-01  73 J449 42.08182 -72.61861 52758
+# 2: 2001 2001-04-01  80 J449 42.05481 -72.62351 52760
+# 3: 2001 2001-08-01  88 I219 42.08514 -72.62851 52761
+# 4: 2001 2001-12-01  98  F03 42.08185 -72.61878 52762
+# 5: 2001 2001-02-01  66  I64 42.08078 -72.59399 52763
+# 6: 2001 2001-07-01  76  F03 42.07990 -72.61476 52764
 
 ## check age ---------------------
 summary(mort[,age])
@@ -156,21 +160,31 @@ dev.off()
 
 ## 3. load ZCTA shape file - the 2010 version ---------------------------------
 point_dt <- st_as_sf(mort, coords = c("long", "lat"), crs = 4326) # spatial data
-download.file("ftp://ftp2.census.gov/geo/tiger/TIGER2010/ZCTA5/2010/tl_2010_25_zcta510.zip", "tl_2010_25_zcta510.zip")
-unzip("tl_2010_25_zcta510.zip")
+# download.file("ftp://ftp2.census.gov/geo/tiger/TIGER2010/ZCTA5/2010/tl_2010_25_zcta510.zip", "tl_2010_25_zcta510.zip")
+# unzip("tl_2010_25_zcta510.zip")
 zcta_shp10 <- st_transform(sf::st_read("tl_2010_25_zcta510.shp"), 4326)
 zcta10 <- st_join(point_dt, zcta_shp10, join = st_intersects)
 setDT(zcta10)
+
+zcta_list <- setDT(zcta_shp10[, "ZCTA5CE10"])[,ZCTA5CE10]
+zcta_list <- rep(zcta_list, 15)
+year_list <- NULL
+for (year_i in 2001:2015) {
+  year_list <- c(year_list, rep(year_i, 538))
+}
+zcta_year <- setDT(data.frame(ZCTA5CE10 = zcta_list, 
+                              year = year_list))
+saveRDS(zcta_year, "zcta_year.rds")
 
 ## 4. aggregate MA death on ZCTA per year -------------------------------
 ## assign ZCTA to each row of death record
 mort_loc <- merge(mort, zcta10[, .(ID, ZCTA5CE10)], by = "ID")
 ## create age label for each group
-mort_loc[,`:=` (age0_65 = age>=0&age<65, 
+mort_loc[,`:=` (age18_65 = age>=18&age<65, 
                 age65_85 = age>=65&age<85, 
                 age85 = age>=85)]
 ## aggregate death records on ZCTA for year and month, and by age
-dt <- mort_loc[, .(ID, year, date, icd, ZCTA5CE10, age0_65, age65_85, age85)]
+dt <- mort_loc[, .(ID, year, date, icd, ZCTA5CE10, age18_65, age65_85, age85)]
 dt[, month := month(date)]
 dt[, `:=` (icd_str1 = substr(icd,1,1),
            icd_str2 = substr(icd,1,2),
@@ -187,7 +201,7 @@ count_all <- dt[, .(CVD = sum(CVD_TF),
                 CHF = sum(CHF_TF),
                 stroke = sum(stroke_TF),
                 TOT = sum(TOT_TF)), by = .(year, month, ZCTA5CE10)]
-count0_65 <- dt[(age0_65), .(CVD = sum(CVD_TF),
+count18_65 <- dt[(age18_65), .(CVD = sum(CVD_TF),
                              MI = sum(MI_TF),
                              CHF = sum(CHF_TF),
                              stroke = sum(stroke_TF),
@@ -208,20 +222,20 @@ count85 <- dt[(age85), .(CVD = sum(CVD_TF),
 sum(count_all[is.na(ZCTA5CE10),TOT])
 # [1] 17 # number of individuals without assigned ZCTA
 final_all <- na.omit(count_all)
-final0_65 <- na.omit(count0_65)
+final18_65 <- na.omit(count18_65)
 final65_85 <- na.omit(count65_85)
 final85 <- na.omit(count85)
 
 sum(final_all[,TOT])
-# [1] 737366
-sum(final0_65[,TOT])
-# [1] 138334
+# [1] 730134
+sum(final18_65[,TOT])
+# [1] 131102
 sum(final65_85[,TOT])
 # [1] 323645
 sum(final85[,TOT])
 # [1] 275387
 
 saveRDS(final_all, "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/MAdeath_count_ZIP_ageall.rds")
-saveRDS(final0_65, "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/MAdeath_count_ZIP_age065.rds")
+saveRDS(final18_65, "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/MAdeath_count_ZIP_age1865.rds")
 saveRDS(final65_85, "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/MAdeath_count_ZIP_age6585.rds")
 saveRDS(final85, "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/MAdeath_count_ZIP_age85.rds")
--- codes/03mergeDatasetsALL.R
+++ codes/03mergeDatasetsALL.R
@@ -15,6 +15,9 @@ setwd("/media/qnap3/Shuxin/ParticalRadiation_MAdeath/")
 library(data.table)
 ## except for census data,data are all from Joel's server
 
+## 0. load zcta and year combination ------------------------------------------
+zcta_year <- readRDS("zcta_year.rds")
+
 ## 1.1 annual death on ZCTA ---------------------------------------------------
 ## 1.1.1 all age ---------
 dir_death_count <- "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/"
@@ -26,23 +29,23 @@ count_year <- count_month[, .(CVD = sum(CVD),
                               TOT = sum(TOT)), by = .(year, ZCTA5CE10)] # aggregate on year
 summary(count_year[,year])
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 2000    2003    2008    2008    2012    2015 
+# 2001    2004    2008    2008    2012    2015 
 sum(count_year[,TOT])
-# [1] 738913
+# [1] 730134
 
-## 1.1.2. age 0-65 ----
+## 1.1.2. age 18-65 ----
 dir_death_count <- "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/"
-count_month065 <- readRDS(paste0(dir_death_count, "MAdeath_count_ZIP_age065.rds")) # this is by year and month
-count_year065 <- count_month065[, .(CVD = sum(CVD),
+count_month1865 <- readRDS(paste0(dir_death_count, "MAdeath_count_ZIP_age1865.rds")) # this is by year and month
+count_year1865 <- count_month1865[, .(CVD = sum(CVD),
                               MI = sum(MI),
                               CHF = sum(CHF),
                               stroke = sum(stroke),
                               TOT = sum(TOT)), by = .(year, ZCTA5CE10)] # aggregate on year
-summary(count_year065[,year])
+summary(count_year1865[,year])
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 2000    2003    2008    2008    2012    2015 
-sum(count_year065[,TOT])
-names(count_year065)[3:7] <- paste0(names(count_year065)[3:7], "065")
+# 2001    2004    2008    2008    2012    2015 
+sum(count_year1865[,TOT])
+names(count_year1865)[3:7] <- paste0(names(count_year1865)[3:7], "065")
 
 ## 1.1.3. age 65-85 ----
 dir_death_count <- "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/"
@@ -54,12 +57,12 @@ count_year6585 <- count_month6585[, .(CVD = sum(CVD),
                                     TOT = sum(TOT)), by = .(year, ZCTA5CE10)] # aggregate on year
 summary(count_year6585[,year])
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 2000    2003    2008    2008    2012    2015 
+# 2001    2004    2008    2008    2012    2015
 sum(count_year6585[,TOT])
-# [1] 738913
+# [1] 323645
 names(count_year6585)[3:7] <- paste0(names(count_year6585)[3:7], "6585")
 
-## 1.1.4. age 0-65 ----
+## 1.1.4. age85+ ----
 dir_death_count <- "/media/qnap3/Shuxin/ParticalRadiation_MAdeath/"
 count_month85 <- readRDS(paste0(dir_death_count, "MAdeath_count_ZIP_age85.rds")) # this is by year and month
 count_year85 <- count_month85[, .(CVD = sum(CVD),
@@ -69,10 +72,16 @@ count_year85 <- count_month85[, .(CVD = sum(CVD),
                                     TOT = sum(TOT)), by = .(year, ZCTA5CE10)] # aggregate on year
 summary(count_year85[,year])
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 2000    2003    2008    2008    2012    2015 
+# 2001    2004    2008    2008    2012    2015 
 sum(count_year85[,TOT])
-# [1] 738913
+# [1] 275387
 names(count_year85)[3:7] <- paste0(names(count_year85)[3:7], "85")
+
+remove(count_month)
+remove(count_month1865)
+remove(count_month6585)
+remove(count_month85)
+gc()
 
 ## 1.2 annual beta radiation on ZCTA ------------------------------------------
 load("MA_ZIP_Annual_Beta_V3.RData") # radiation data from Longxiang
@@ -83,7 +92,6 @@ gc()
 summary(betaR[,Year])
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 # 2001    2005    2009    2009    2013    2017 
-names(count_year065)[3:7] <- paste0(names(count_year065)[3:7], "065")
 
 ## ** all following data should be 2001-2015 ---------------------------------
 ## 2.1 seasonal temperature on ZCTA ------------------------------------------
@@ -114,27 +122,31 @@ pcount_long[, year:= substr(year, 6, 9)][]
 pcount_long[, year:= as.integer(year)]
 names(pcount_long)[1] <- "ZCTA5"
 
-## 2.4 other census variables -------------------------------------------------------
+## 2.4 other census variables -----------------------------------------------------
 census_ses <- readRDS("census0115.rds")
 census_ses <- census_ses[(zip %in% count_year[, ZCTA5CE10]),]
 
-## 3. merge all together --------
-## 3.1 merge exposure and health outcome -----
-dt <- merge(count_year, betaR, by.x = c("year", "ZCTA5CE10"), by.y = c("Year", "ZIP"), all.x = TRUE) # X and Y from 2001 to 2015
+## 3. merge on zcta and year combination ---------------------------------------
+## 3.1 merge count and exposure --------
+## merge count
+dt <- merge(zcta_year, count_year, by = c("year", "ZCTA5CE10"), all.x = T)
+dt <- dt[,lapply(.SD,function(x){ifelse(is.na(x),0,x)})]
+## merge beta exposure
+dt <- merge(dt, betaR, by.x = c("year", "ZCTA5CE10"), by.y = c("Year", "ZIP"), all.x = TRUE) # X and Y from 2001 to 2015
 dim(dt)
-# [1] 7703    8
+# [1] 8070    8
 sum(dt[is.na(dt[,Beta]), TOT])
-# [1] 10652 # number of individuals without Beta info
+# [1] 10532 # number of individuals without Beta info
 sum(dt[, TOT])
 # [1] 738896
 sum(dt[is.na(dt[,Beta]), TOT])/sum(dt[, TOT])
-# [1] 0.0144161
+# [1] 0.01442475
 
 sum(is.na(dt[,Beta]))
-# [1] 528
+# [1] 765
 dt <- na.omit(dt)
 sum(dt[, TOT])
-# [1] 728244
+# [1] 719602
 
 ## 3.2 merge covariates -----
 dt <- merge(dt, seasonaltemp, by.x = c("year", "ZCTA5CE10"), by.y = c("year", "zcta"), all.x = TRUE)
@@ -142,22 +154,23 @@ dt <- merge(dt, pm25, by.x = c("year", "ZCTA5CE10"), by.y = c("year", "ZIP"), al
 dt <- merge(dt, pcount_long, by.x = c("year", "ZCTA5CE10"), by.y = c("year", "ZCTA5"), all.x = TRUE)
 dt <- merge(dt, census_ses, by.x = c("year", "ZCTA5CE10"), by.y = c("year", "zip"), all.x = TRUE)
 
-sum(dt[!complete.cases(dt), TOT]) # 2975 people without covariates info
-sum(dt[!complete.cases(dt), TOT])/sum(dt[, TOT]) # [1] 0.004085169
+sum(dt[!complete.cases(dt), TOT]) # 2949 people without covariates info
+sum(dt[!complete.cases(dt), TOT])/sum(dt[, TOT]) # [1] 0.004098099
 
 final <- na.omit(dt)
 sum(final[,TOT])
-# [1] 725269
+# [1] 716653
 dim(final)
-# [1] 6985   21
+# [1] 7054   21
 #saveRDS(final, "finalDT.rds")
 
 #final_allage <- readRDS("finalDT.rds")
 names(final)
-final <- merge(final, count_year065, by = c("year", "ZCTA5CE10"), all.x = T)
+final <- merge(final, count_year1865, by = c("year", "ZCTA5CE10"), all.x = T)
 final <- merge(final, count_year6585, by = c("year", "ZCTA5CE10"), all.x = T)
 final <- merge(final, count_year85, by = c("year", "ZCTA5CE10"), all.x = T)
 final
+anyNA(final)
 final <- final[,lapply(.SD,function(x){ifelse(is.na(x),0,x)})] # convert NA to 0
 anyNA(final)
 final
